ba1377e910a09a5a62a5e50f37098d636a3e9632
Sergey Melnichenko <159032191+SerhiiMelnichenko@users.noreply.github.com>
2024-06-13 21:53:11 +0300

    task 1-2-3-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,17 @@
 
 const sortByDescendingFriendCount = (users) => {
-    return users.toSorted((a, b) => b.localeCompare(a)) 
+    return users.toSorted((a, b) => b.friends.length - a.friends.length) 
     
 }
  
+//Напиши стрілочну функцію sortByDescendingFriendCount(users),
+//яка прийматиме один параметр users — масив об’єктів користувачів.
+//Функція має повертати масив усіх користувачів,
+//відсортованих за спаданням кількостій їх друзів(властивість friends).
+//Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+//У консоль будуть виведені результати її роботи.
+
+
 
 
 console.log(
@@ -45,43 +53,44 @@ console.log(
     }
   ])
 )
-;[
-  {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    gender: "male"
-  },
-  {
-    name: "Sharlene Bush",
-    friends: ["Briana Decker", "Sharron Pace"],
-    gender: "female"
-  },
-  {
-    name: "Elma Head",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    gender: "female"
-  },
-  {
-    name: "Carey Barr",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    gender: "male"
-  },
-  {
-    name: "Blackburn Dotson",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    gender: "male"
-  },
-  {
-    name: "Sheree Anthony",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    gender: "female"
-  },
-  {
-    name: "Moore Hensley",
-    friends: ["Sharron Pace"],
-    gender: "male"
-  }
-]
+    
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
 
 
 
